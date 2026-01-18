c260fc20f1deaea2fc9af2f9fa9cb82a3d682de5
Andrew Gambino <ang525@pitt.edu>
2026-01-17 22:38:26 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,19 @@
+// Duration in minutes
+const BLOCK_DURATION = 1; // 1 hour
 
+// Enable the ruleset immediately
+chrome.declarativeNetRequest.updateEnabledRulesets({
+  enableRulesetIds: ["main_rules"]
+});
+
+// Set an alarm to disable it later
+chrome.alarms.create("disableBlock", { delayInMinutes: BLOCK_DURATION });
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === "disableBlock") {
+    chrome.declarativeNetRequest.updateEnabledRulesets({
+      disableRulesetIds: ["main_rules"]
+    });
+    console.log("Temporary block expired, rules disabled.");
+  }
+});
