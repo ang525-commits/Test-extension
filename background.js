0a09994edd4342a1397f3fc6628ac265b14611b8
Andrew Gambino <ang525@pitt.edu>
2026-01-17 23:02:54 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,8 @@
-// Duration in minutes
-const BLOCK_DURATION = 1; // 1 hour
-
-// Enable the ruleset immediately
+const BLOCK_DURATION = 1;
 chrome.declarativeNetRequest.updateEnabledRulesets({
   enableRulesetIds: ["main_rules"]
 });
-
-// Set an alarm to disable it later
 chrome.alarms.create("disableBlock", { delayInMinutes: BLOCK_DURATION });
-
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === "disableBlock") {
     chrome.declarativeNetRequest.updateEnabledRulesets({
