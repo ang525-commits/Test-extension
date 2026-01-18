7ed98049855c9c80afff80184d9b51b043baabe2
Andrew Gambino <ang525@pitt.edu>
2026-01-17 23:23:14 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,15 +1,21 @@
 const RULESET_ID = "ruleset_1"; 
-chrome.declarativeNetRequest.updateEnabledRulesets({
-  enableRulesetIds: [RULESET_ID]
-}).then(() => {
-  console.log("Block enabled for 1 minute.");
-  chrome.alarms.create("disableBlock", { delayInMinutes: 1 });
-}).catch(console.error);
+let USERCHOICE = 0;
+let isBlocked = false;
+if(!isBlocked){
+  let duration = prompt("Block for how many minutes?");
+    if (duration > 0) {
+        enableRuleset();
+        set blockedUntil = now + duration;
+        chrome.alarms.create("disableBlock", { delayInMinutes: duration });
+        isBlocked = true;
+    }
+}
 chrome.alarms.onAlarm.addListener(async (alarm) => {
   if (alarm.name === "disableBlock") {
     try {
       await chrome.declarativeNetRequest.updateEnabledRulesets({
         disableRulesetIds: [RULESET_ID]
+        isBlocked = false;
       });
       console.log("Temporary block expired, rules disabled.");
     } catch (err) {
