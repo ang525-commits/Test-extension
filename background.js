5e076b73351f7a187e8a103bac939d009e392b02
Andrew Gambino <ang525@pitt.edu>
2026-01-23 12:19:03 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,15 +1,52 @@
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   const ruleId = 1; 
   const rule = {
-    id: ruleId,
-    priority: 1,
-    action: { type: 'block' },
-    condition: { urlFilter: msg.site, resourceTypes: ['main_frame'] }
+    "id": 1,
+    "priority": 1,
+    "action": {
+      "type": "redirect",
+      "redirect": {
+         "extensionPath": "/Shame.html"
+      }
+    },
+    "condition": {
+      "urlFilter": "claude.ai",
+      "resourceTypes": ["main_frame"]
+    }
   };
-
+  const ruleId2 = 1;
+  const rule2 = {
+    "id": 2,
+    "priority": 1,
+    "action": {
+      "type": "redirect",
+      "redirect": {
+         "extensionPath": "/Shame.html"
+      }
+    },
+    "condition": {
+      "urlFilter": "gemini.google.com",
+      "resourceTypes": ["main_frame"]
+    }
+  };
+  const ruleId3 = 1;
+  const rule3 = {
+    "id": 2,
+    "priority": 1,
+    "action": {
+      "type": "redirect",
+      "redirect": {
+         "extensionPath": "/Shame.html"
+      }
+    },
+    "condition": {
+      "urlFilter": "chatgpt.com",
+      "resourceTypes": ["main_frame"]
+    }
+  };
   if (msg.action === 'activateRule') {
-    chrome.declarativeNetRequest.updateDynamicRules({ addRules: [rule] });
+    chrome.declarativeNetRequest.updateDynamicRules({ addRules: [rule,rule2,rule3] });
   } else if (msg.action === 'deactivateRule') {
-    chrome.declarativeNetRequest.updateDynamicRules({ removeRuleIds: [ruleId] });
+    chrome.declarativeNetRequest.updateDynamicRules({ removeRuleIds: [ruleId,ruleId2,ruleId3] });
   }
 });
