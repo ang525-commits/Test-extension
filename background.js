1d34415b710e77bef953198d121f6e1051f10fc6
Andrew Gambino <ang525@pitt.edu>
2026-01-23 12:14:41 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,8 +1,15 @@
-chrome.webNavigation.onCompleted.addListener(
-  async () => {
-    await chrome.action.openPopup();
-  },
-  { url: [
-    { urlMatches: 'https://www.w3schools.com/howto/howto_css_modals.asp' },
-  ] },
-);
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  const ruleId = 1; 
+  const rule = {
+    id: ruleId,
+    priority: 1,
+    action: { type: 'block' },
+    condition: { urlFilter: msg.site, resourceTypes: ['main_frame'] }
+  };
+
+  if (msg.action === 'activateRule') {
+    chrome.declarativeNetRequest.updateDynamicRules({ addRules: [rule] });
+  } else if (msg.action === 'deactivateRule') {
+    chrome.declarativeNetRequest.updateDynamicRules({ removeRuleIds: [ruleId] });
+  }
+});
